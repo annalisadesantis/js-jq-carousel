46bb0dc1543b5898a049c916976f976037f348e9
Annalisa <annalisadesantis770@gmail.com>
2020-10-27 16:16:00 +0100

    carosello  + js prev and next

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+    // Click per andare avanti
+    $('.next i').click(function() {
+        // creo una varibile per salvare l'immagine corrente
+        var img_corrente = $('img.active');
 
+        img_corrente.removeClass('active');
+
+        // verifico se esiste un'immagine successiva
+        if(img_corrente.next('img').length) {
+            img_corrente.next('img').addClass('active');
+        } else {
+            $('.painting :first-child').addClass('active');
+        }
+    });
+
+    // Click per andare indietro
+    $('.prev i').click(function() {
+        // creo una varibile per salvare l'immagine corrente
+        var img_corrente = $('img.active');
+
+        img_corrente.removeClass('active');
+
+        // verifico se esiste un'immagine precedente
+        if(img_corrente.prev('img').length) {
+            img_corrente.prev('img').addClass('active');
+        } else {
+            $('.painting img:last-of-type').addClass('active');
+        }
+    });
 
 });
