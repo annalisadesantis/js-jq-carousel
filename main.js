9c266f2d6c813438108d027bf77f071b966e8517
Annalisa <annalisadesantis770@gmail.com>
2020-10-28 13:24:19 +0100

    aggiunta click su pallini

--- main.js
+++ main.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
     $('.next i').click(function() {
         // creo una varibile per salvare l'immagine corrente
         var img_corrente = $('img.active');
+        // creo una varibile per salvare l'icona corrente
+        var icona_corrente = $('span.active');
+
 
         img_corrente.removeClass('active');
+        icona_corrente.removeClass('active');
 
         // verifico se esiste un'immagine successiva
         if(img_corrente.next('img').length) {
             img_corrente.next('img').addClass('active');
+            icona_corrente.next('span').addClass('active');
         } else {
-            $('.painting :first-child').addClass('active');
+            $('.painting img:first-child').addClass('active');
+            $('.bullets span:first-child').addClass('active');
         }
     });
 
@@ -19,31 +25,42 @@ $(document).ready(function() {
     $('.prev i').click(function() {
         // creo una varibile per salvare l'immagine corrente
         var img_corrente = $('img.active');
+        // creo una varibile per salvare l'icona corrente
+        var icona_corrente = $('span.active');
 
         img_corrente.removeClass('active');
+        icona_corrente.removeClass('active');
 
         // verifico se esiste un'immagine precedente
         if(img_corrente.prev('img').length) {
             img_corrente.prev('img').addClass('active');
+            icona_corrente.prev('span').addClass('active');
         } else {
             $('.painting img:last-of-type').addClass('active');
+            $('.bullets span:last-child').addClass('active');
         }
     });
 
-    // Click per andare avanti e indietro con il bullets
+    // intercetto il click su un pallino
     $('.bullets span').click(function() {
-        // creo una varibile per salvare lo span corrente
-        var icona_corrente = $('span.active');
+    // recupero il pallino corrente
+    var pallino_corrente = $('span.active');
+    // tolgo la classe active al pallino corrente
+    pallino_corrente.removeClass('active');
+    // aggiungo la classe active al pallino su cui l'utente ha cliccato
+    $(this).addClass('active');
 
-        icona_corrente.removeClass('active');
 
-        // verifico se esiste uno span
-        if(icona_corrente.next('span').length) {
-            icona_corrente.next('span').addClass('active');
-        } else {
-            $('.bullets :first-child').addClass('active');
-        }
+    // recupero l'immagine corrente
+    var img_corrente = $('img.active');
+    // tolgo la classe active all'immagine corrente
+    img_corrente.removeClass('active');
+    // recupero il numero del pallino su cui l'utente ha cliccato
+    var indice_pallino = $(this).index();
+    // recupero l'immagine corrispondente allo stesso numero
+    var nuova_img = $('.painting img').eq(indice_pallino);
+    // assegno la classe active alla nuova immagine
+    nuova_img.addClass('active');
     });
-
 
 });
